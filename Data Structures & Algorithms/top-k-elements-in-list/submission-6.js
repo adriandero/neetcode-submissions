b07dd680f14e1ff016aaed8143178445687b1229
Adrian-Nikola De Ro <62173065+adriandero@users.noreply.github.com>
2026-05-11 11:53:04 +0200

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let map = new Map();
+
+        for (const num of nums) {
+            map.set(num, (map.get(num) || 0) + 1);
+        }
+
+        let arr = new Array(nums.length + 1).fill(null).map(() => []);
+
+        for (const [key, val] of map) {
+            arr[val].push(key);
+        }
+
+        let final = [];
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] && arr[i].length > 0) {
+                for (let val of arr[i]) {
+                    final.push(val);
+                    if (final.length === k) return final;
+                }
+            }
+        }
+        return final;
+    }
+}
