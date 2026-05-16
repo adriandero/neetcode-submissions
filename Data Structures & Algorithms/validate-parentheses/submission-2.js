67f24ee7835fa3dc17ebf5b3cf1d794c5aeb8466
Adrian-Nikola De Ro <62173065+adriandero@users.noreply.github.com>
2026-05-16 10:07:30 +0200

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,57 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+
+        for (const br of s) {
+            const stackTop = stack[stack.length - 1];
+
+            switch (br) {
+                case "}": {
+                    if (stackTop != "{") return false;
+                    stack.pop();
+                    break;
+                }
+                case ")": {
+                    if (stackTop != "(") return false;
+                    stack.pop();
+                    break;
+                }
+                case "]": {
+                    if (stackTop != "[") return false;
+                    stack.pop();
+                    break;
+                }
+                default: {
+                    stack.push(br);
+                }
+            }
+        }
+
+        return stack.length === 0;
+    }
+}
+
+/**
+ * ()[]
+ * {()}{[]}[]
+ * {(})
+ *
+ * if you have a open bracket, and try to find your closing bracket - in between should always be an even number of char
+ *
+ * so you could brute force it like that - go through the array, if there is a open bracket, go through the rest of the aray, to find your closing bracket and count if the number inbetween is even
+ * wait, nevermind - i think this is a possible case where this would break: {((}
+ *
+ * open bracket -> closed bracket
+ * is a bracket currently open, if yes -> you can only add a open bracket to it or, that corresponding closing bracket
+ * so you can have a stack, that has the latest bracket and if the next pracket is a closing bracket check if it is the corresponding one - if not flase, if yes, pop the stack of the bracket you just checked
+ * should be O(n)
+ * -
+ * -
+ * -
+ * -
+ * -
+ */
